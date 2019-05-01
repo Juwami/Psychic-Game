683bea135c3943dc9218bb43b8a32cebcbc88d21
Juwami <sean.nguyenf@yahoo.com>
2019-04-30 21:25:42 -0400

    added selfhelp comments to js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,9 @@ let losses = 0
 let guessesLeft = 10
 
 let alphabet = ['a','b','c','d','e','f','g','h','i','g','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+// array of all letters of alphabet
 let guesses = []
+// open ended array of guesses - will need to add it
 
 let winsText = document.getElementById('wins-text')
 let lossesText = document.getElementById('losses-text')
@@ -18,15 +20,18 @@ document.onkeypress = function(event) {
         else {
         if (userGuess === alphabetRandom) {
             wins++
+            // wins++ adds to wins after userGuess equals the designated alphabet letter
             alert('You won!')
             guessesLeft = 10
             guesses.length = 0
         } else {
             if (guesses.includes(userGuess)) {
+                // checks the array to see if userGuess is listed - with assistance of David
                 alert('Already guessed. Try again')
             } else {
                 guessesLeft--
                 guesses.push(userGuess)
+                // adds userGuess to the list of guesses in array
                 if (guessesLeft === 0) {
                     losses++
                     alert('You lost!')
@@ -36,6 +41,7 @@ document.onkeypress = function(event) {
             }
         }
     }
+    // below allows javascript to edit html 
     winsText.textContent = wins
     lossesText.textContent = losses
     guessesLeftText.textContent = guessesLeft
