5734e9b497197fe2313d7ae4829b28f97c7e6007
Juwami <sean.nguyenf@yahoo.com>
2019-05-04 21:19:37 -0400

    had to add j

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@ let wins = 0
 let losses = 0
 let guessesLeft = 10
 
-let alphabet = ['a','b','c','d','e','f','g','h','i','g','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+let alphabet = ['a','b','c','d','e','f','g','h','i','j','g','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 // array of all letters of alphabet
 let guesses = []
 // open ended array of guesses - will need to add it
