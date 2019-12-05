9519ef8ad570667ec9c45d2691fef0aefe66bbea
Juwami <sean.nguyenf@yahoo.com>
2019-12-04 23:13:52 -0500

    install react-router-dom, creating homepage

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,9 +1,14 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
+import { Route, Link } from 'react-router-dom';
+import Homepage from '../features/home/Homepage'
 
 function App() {
 	return (
 		<Fragment>
-			<h1>The Psychic Game</h1>
+			<Route path="/" component={Homepage}/>
+			{/* <Header as="h1">
+				<h1>The Psychic Game</h1>
+			</Header>
 			<p>Guess what letter I'm thinking of</p>
 			<p>Wins:</p>
 			<p id="wins-text">0</p>
@@ -12,7 +17,7 @@ function App() {
 			<p>Guesses Left:</p>
 			<p id="guesses-left">10</p>
 			<p>Your Guesses so far:</p>
-			<p id="guesses" />
+	<p id="guesses" /> */}
 		</Fragment>
 	);
 }
